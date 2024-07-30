f369a5db6882df204e9fd8bdf85bad1b86406e77
Kevin Wang <kangxu@umich.edu>
2024-07-30 00:08:21 -0700

    Initial setup with template

---
+++ src/data/site-config.ts
@@ -0,0 +1,109 @@
+export type Image = {
+    src: string;
+    alt?: string;
+    caption?: string;
+};
+
+export type Link = {
+    text: string;
+    href: string;
+};
+
+export type Hero = {
+    title?: string;
+    text?: string;
+    image?: Image;
+    actions?: Link[];
+};
+
+export type Subscribe = {
+    title?: string;
+    text?: string;
+    formUrl: string;
+};
+
+export type SiteConfig = {
+    logo?: Image;
+    title: string;
+    subtitle?: string;
+    description: string;
+    image?: Image;
+    headerNavLinks?: Link[];
+    footerNavLinks?: Link[];
+    socialLinks?: Link[];
+    hero?: Hero;
+    subscribe?: Subscribe;
+    postsPerPage?: number;
+    projectsPerPage?: number;
+};
+
+const siteConfig: SiteConfig = {
+    title: 'Kilobite',
+    subtitle: 'Satisfying your tech appetite, one bite at a time.',
+    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+    image: {
+        src: '/dante-preview.jpg',
+        alt: 'Dante - Astro.js and Tailwind CSS theme'
+    },
+    headerNavLinks: [
+        {
+            text: 'Home',
+            href: '/'
+        },
+        // {
+        //     text: 'Projects',
+        //     href: '/projects'
+        // },
+        {
+            text: 'Blog',
+            href: '/blog'
+        },
+        {
+            text: 'Tags',
+            href: '/tags'
+        }
+    ],
+    footerNavLinks: [
+        // {
+        //     text: 'About',
+        //     href: '/about'
+        // },
+        {
+            text: 'Contact',
+            href: '/contact'
+        },
+        {
+            text: 'GitHub',
+            href: 'https://github.com/jogjech'
+        }
+    ],
+    socialLinks: [
+        // {
+        //     text: 'GitHub',
+        //     href: 'https://github.com/jogjech'
+        // }
+    ],
+    hero: {
+        title: 'Hungry of learning?',
+        text: "I'm **Kevin Wang**, a Senior Software Developer at Amazon.",
+        image: {
+            src: '/hero.jpeg',
+            alt: 'A person sitting at a desk in front of a computer'
+        },
+        actions: [
+            {
+                text: 'Get in Touch',
+                href: '/contact'
+            }
+        ]
+    },
+    subscribe: {
+        title: 'Subscribe to the Kilobite Newsletter',
+        text: 'One update per week. All the latest posts directly in your inbox.',
+        formUrl: '#'
+    },
+    postsPerPage: 8,
+    projectsPerPage: 8
+};
+
+export default siteConfig;
